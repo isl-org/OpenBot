9f2cc380c4e70a6a9c9ec264fef27885a1240200
Matthias Mueller <thias15@web.de>
2020-11-16 15:59:19 +0100

    fix control in person following for negative sensor rotation

--- android/app/src/main/java/org/openbot/CameraActivity.java
+++ android/app/src/main/java/org/openbot/CameraActivity.java
@@ -978,7 +978,7 @@ public abstract class CameraActivity extends AppCompatActivity
   protected void sendControlToVehicle(ControlSignal vehicleControl) {
     if ((usbConnection != null) && usbConnection.isOpen() && !usbConnection.isBusy()) {
       String message =
-          String.format(
+          String.format(Locale.US,
               "c%d,%d\n",
               (int) (vehicleControl.getLeft() * speedMultiplier),
               (int) (vehicleControl.getRight() * speedMultiplier));
@@ -988,7 +988,7 @@ public abstract class CameraActivity extends AppCompatActivity
 
   protected void sendIndicatorToVehicle(int vehicleIndicator) {
     if (usbConnection != null && usbConnection.isOpen() && !usbConnection.isBusy()) {
-      String message = String.format("i%d\n", vehicleIndicator);
+      String message = String.format(Locale.US, "i%d\n", vehicleIndicator);
       usbConnection.send(message);
     }
   }
