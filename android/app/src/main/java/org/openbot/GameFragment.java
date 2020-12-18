4a7229701965c4f6754e482d3f9dc6123d3f6570
Dhruv <dhruv2295@gmail.com>
2020-12-18 20:14:10 +0530

    game fragment creation

---
+++ android/app/src/main/java/org/openbot/GameFragment.java
@@ -0,0 +1,26 @@
+package org.openbot;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.jetbrains.annotations.NotNull;
+
+public class GameFragment extends CameraFragment {
+
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container,
+							 Bundle savedInstanceState) {
+		return inflateFragment(R.layout.fragment_game,inflater,container);
+	}
+}
