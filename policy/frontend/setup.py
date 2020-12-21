beca2b05a8def6936510fab1e1887800a1c5ed00
Sándor Balázs <balazs.sandor@vcc.live>
2020-12-21 23:54:38 +0100

    Update policy readme with web app

--- policy/frontend/setup.py
+++ policy/frontend/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="openbot_frontend",
-    version="0.1.0",
+    version="0.2.0",
     description="OpenBot Frontend package",
     url="https://github.com/sanyatuning/OpenBot",
     author="Balazs Sandor",
