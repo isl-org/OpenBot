9c53914e07f558e0b647b17987b691fa26bc3ed1
Sándor Balázs <balazs.sandor@vcc.live>
2021-01-02 21:47:48 +0100

    bump frontend version

--- policy/frontend/setup.py
+++ policy/frontend/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="openbot_frontend",
-    version="0.2.0",
+    version="0.3.0",
     description="OpenBot Frontend package",
     url="https://github.com/sanyatuning/OpenBot",
     author="Balazs Sandor",
