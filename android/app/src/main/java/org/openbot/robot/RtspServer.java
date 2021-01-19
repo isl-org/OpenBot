67a388cecbd89b9ca8dca1aee97d6cc7ee0fa191
Ivo Zivkov <ivo.zivkov2@ontario.ca>
2021-01-19 12:50:36 -0500

    WIP, video streaming

---
+++ android/app/src/main/java/org/openbot/robot/RtspServer.java
@@ -0,0 +1,154 @@
+package org.openbot.robot;
+
+import android.graphics.SurfaceTexture;
+import android.media.ToneGenerator;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.TextureView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.pedro.rtplibrary.rtsp.RtspCamera2;
+import com.pedro.rtsp.utils.ConnectCheckerRtsp;
+import com.pedro.rtspserver.RtspServerCamera1;
+import com.pedro.rtspserver.RtspServerCamera2;
+
+import org.openbot.R;
+import org.openbot.customview.AutoFitTextureView;
+
+public class RtspServer implements ConnectCheckerRtsp, TextureView.SurfaceTextureListener {
+    private final String TAG = "RtspServer";
+    private RtspServerCamera1 rtspServerCamera1;
+    private AutoFitTextureView textureView;
+
+    public RtspServer() {
+    }
+
+    public void startServer(int width, int height, int port) {
+        if (rtspServerCamera1 == null) {
+            rtspServerCamera1 = new RtspServerCamera1(textureView, this, port);
+        }
+
+        /* Working params:
+        {
+      int32_t color-format = 21
+      int32_t i-frame-interval = 2
+      string mime = "video/avc"
+      int32_t width = 480
+      int32_t bitrate = 1228800
+      int32_t max-input-size = 0
+      int32_t frame-rate = 30
+      int32_t height = 640
+      int32_t encoder = 1
+        }
+         */
+        if (!rtspServerCamera1.isStreaming()) {
+            if (rtspServerCamera1.prepareAudio()
+                    // && rtspServerCamera1.prepareVideo()) {
+                    && rtspServerCamera1.prepareVideo(width, height, 30, 5000 * 1024, 2, 90)) {
+                rtspServerCamera1.startStream("");
+            }
+        }
+    }
+
+    public void startServer() {
+        startServer(1920, 1080, 1935);
+    }
+
+    public void stopServer() {
+        if (rtspServerCamera1 != null) {
+            if (rtspServerCamera1.isRecording()) {
+                rtspServerCamera1.stopRecord();
+            }
+
+            if (rtspServerCamera1.isStreaming()) {
+                rtspServerCamera1.stopStream();
+            }
+
+            // rtspServerCamera1.stopPreview();
+            rtspServerCamera1 = null;
+        }
+    }
+
+    public void init() {
+    }
+
+    protected void setView(AutoFitTextureView textureView) {
+        this.textureView = textureView;
+    }
+
+    @Override
+    public void onConnectionSuccessRtsp() {
+
+        Log.i(TAG, "onConnectionSuccessRtsp");
+    }
+
+    @Override
+    public void onConnectionFailedRtsp(final String reason) {
+        rtspServerCamera1.stopStream();
+        beep();
+    }
+
+    @Override
+    public void onNewBitrateRtsp(long bitrate) {
+        Log.i(TAG, "Bitrate set to " + bitrate);
+        // do nothing
+    }
+
+    @Override
+    public void onDisconnectRtsp() {
+        // do nothing
+        beep();
+    }
+
+    @Override
+    public void onAuthErrorRtsp() {
+
+        beep();
+    }
+
+    @Override
+    public void onAuthSuccessRtsp() {
+    }
+
+    @Override
+    public void onSurfaceTextureAvailable(@NonNull SurfaceTexture surface, int width, int height) {
+//        if (rtspServerCamera1 == null) {
+//            rtspServerCamera1 = new RtspServerCamera1(textureView, this, 1935);
+//        }
+    }
+
+    @Override
+    public void onSurfaceTextureSizeChanged(@NonNull SurfaceTexture surface, int width, int height) {
+        //textureView.setAspectRatio(width, height);
+        //rtspServerCamera1.startPreview();
+    }
+
+    @Override
+    public boolean onSurfaceTextureDestroyed(@NonNull SurfaceTexture surface) {
+        try {
+            if (rtspServerCamera1.isRecording()) {
+                rtspServerCamera1.stopRecord();
+            }
+            if (rtspServerCamera1.isStreaming()) {
+                rtspServerCamera1.stopStream();
+            }
+        } catch (IllegalStateException e) {
+            Log.e(TAG, e.toString());
+        }
+        rtspServerCamera1.stopPreview();
+        return true;
+    }
+
+    @Override
+    public void onSurfaceTextureUpdated(@NonNull SurfaceTexture surface) {
+        beep();
+    }
+
+    private void beep() {
+        final ToneGenerator tg = new ToneGenerator(6, 100);
+        tg.startTone(ToneGenerator.TONE_CDMA_ALERT_NETWORK_LITE);
+    }
+}
---
+++ android/controller/src/main/java/org/openbot/controller/customComponents/VideoView.kt
@@ -0,0 +1,113 @@
+/*
+ * Developed for the OpenBot project (https://openbot.org) by:
+ *
+ * Ivo Zivkov
+ * izivkov@gmail.com
+ *
+ * Date: 2020-12-27, 10:56 p.m.
+ */
+
+package org.openbot.controller.customComponents
+
+import android.content.Context
+import android.util.AttributeSet
+import android.util.Log
+import android.view.SurfaceView
+import com.pedro.vlc.VlcListener
+import com.pedro.vlc.VlcVideoLibrary
+import org.json.JSONObject
+import org.openbot.controller.StatusEventBus
+import org.openbot.controller.databinding.ActivityFullscreenBinding
+import java.util.*
+
+class VideoView @JvmOverloads constructor(
+        context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : SurfaceView(context, attrs, defStyleAttr) {
+
+    private val TAG:String = "VideoView"
+    private var ipAddress:String? = null
+    private var ipPort:String? = null
+
+    init {
+        StatusEventBus.addSubject("IP_ADDRESS")
+        StatusEventBus.getProcessor("IP_ADDRESS")?.subscribe {
+            gotIpAddress(it as String)
+        }
+
+        StatusEventBus.addSubject("PORT")
+        StatusEventBus.getProcessor("PORT")?.subscribe {
+            gotPort(it as String)
+        }
+
+        StatusEventBus.addSubject("CAMERA_RESOLUTION")
+        StatusEventBus.getProcessor("CAMERA_RESOLUTION")?.subscribe {
+            gotCameraResolution(it as String)
+        }
+    }
+
+    private fun gotIpAddress(address: String) {
+        if (ipPort != null) {
+            start("rtsp://${address}:${ipPort}")
+        }
+        else ipAddress = address;
+    }
+
+    private fun gotPort(port: String) {
+        if (ipAddress != null) {
+            start("rtsp://${ipAddress}:${port}")
+        }
+        else ipPort = port;
+    }
+
+    private fun gotCameraResolution(data: String) {
+        val dimensionJson = JSONObject(data)
+        val width = dimensionJson.getInt("width")
+        val height = dimensionJson.getInt("height")
+        setMeasuredDimension(width, height)
+    }
+
+    private inner class Listener : VlcListener {
+        override fun onComplete() {
+            // Toast.makeText(this, "Playing", Toast.LENGTH_SHORT).show()
+            Log.i(TAG, "onComplete")
+        }
+
+        override fun onError() {
+            // Toast.makeText(this, "Error, make sure your endpoint is correct", Toast.LENGTH_SHORT).show()
+            vlcVideoLibrary?.stop()
+        }
+    }
+
+    private var vlcVideoLibrary: VlcVideoLibrary? = null
+    private val listener = Listener()
+    private val options = arrayOf(":fullscreen")
+
+    init {
+    }
+
+    fun init(binding: ActivityFullscreenBinding) {
+        val args = ArrayList<String>()
+        args.add("-vvv")
+
+        vlcVideoLibrary = VlcVideoLibrary(context, listener, binding.videoView)
+    }
+
+    fun show() {
+        visibility = VISIBLE
+    }
+
+    fun hide() {
+        visibility = INVISIBLE
+    }
+
+    fun release() {
+        stop()
+    }
+
+    fun start(url: String) {
+        vlcVideoLibrary!!.play(url)
+    }
+
+    fun stop() {
+    }
+}
