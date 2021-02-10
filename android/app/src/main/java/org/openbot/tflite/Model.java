0031cbe98b9f9e97b603bf2e8fde7c2aa577113c
Matthias Mueller <thias15@web.de>
2021-02-10 12:54:36 +0100

    make autopilot the first/default model

--- android/app/src/main/java/org/openbot/tflite/Model.java
+++ android/app/src/main/java/org/openbot/tflite/Model.java
@@ -6,26 +6,26 @@ import androidx.annotation.NonNull;
 /** The model. */
 public class Model {
   public enum ID {
+    AUTOPILOT_F,
     DETECTOR_V1_1_0_Q,
     DETECTOR_V3_S_Q,
-    AUTOPILOT_F,
   }
 
+  public static final Model AUTOPILOT_F = new Model(ID.AUTOPILOT_F);
   public static final Model DETECTOR_V1_1_0_Q = new Model(ID.DETECTOR_V1_1_0_Q);
   public static final Model DETECTOR_V3_S_Q = new Model(ID.DETECTOR_V3_S_Q);
-  public static final Model AUTOPILOT_F = new Model(ID.AUTOPILOT_F);
 
   public final ID id;
   public final String filename;
 
   public static Model fromId(String id) {
     switch (ID.valueOf(id)) {
+      case AUTOPILOT_F:
+        return AUTOPILOT_F;
       case DETECTOR_V1_1_0_Q:
         return DETECTOR_V1_1_0_Q;
       case DETECTOR_V3_S_Q:
         return DETECTOR_V3_S_Q;
-      case AUTOPILOT_F:
-        return AUTOPILOT_F;
     }
     throw new IllegalArgumentException("No model with id " + id);
   }
@@ -42,12 +42,12 @@ public class Model {
 
   public static Size getCroppedImageSize(String id) {
     switch (ID.valueOf(id)) {
+      case AUTOPILOT_F:
+        return new Size(256, 96);
       case DETECTOR_V1_1_0_Q:
         return new Size(300, 300);
       case DETECTOR_V3_S_Q:
         return new Size(320, 320);
-      case AUTOPILOT_F:
-        return new Size(256, 96);
     }
     throw new IllegalArgumentException("No size with id " + id);
   }
