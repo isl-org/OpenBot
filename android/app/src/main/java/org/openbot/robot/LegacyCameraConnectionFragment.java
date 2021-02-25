cebdd217873a8c90faa073e8feed16b95b969b79
Dhruv <dhruv2295@gmail.com>
2021-02-26 00:03:34 +0530

    LegacyCameraConnectionFragment crash fix

--- android/app/src/main/java/org/openbot/robot/LegacyCameraConnectionFragment.java
+++ android/app/src/main/java/org/openbot/robot/LegacyCameraConnectionFragment.java
@@ -161,7 +161,7 @@ public class LegacyCameraConnectionFragment extends Fragment {
     // the SurfaceTextureListener).
 
     if (textureView.isAvailable()) {
-      camera.startPreview();
+      if (camera != null) camera.startPreview();
     } else {
       textureView.setSurfaceTextureListener(surfaceTextureListener);
     }
