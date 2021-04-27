9215536e01d7ace92ade6e2cc43fd0a1e8eb1b68
Matthias Mueller <thias15@web.de>
2021-04-27 12:14:41 +0200

    Split Utils class + new CameraUtils class

---
+++ android/app/src/main/java/org/openbot/utils/FormatUtils.java
@@ -0,0 +1,15 @@
+package org.openbot.utils;
+
+public class FormatUtils {
+  public static boolean isNumeric(String strNum) {
+    if (strNum == null) {
+      return false;
+    }
+    try {
+      double d = Double.parseDouble(strNum);
+    } catch (NumberFormatException nfe) {
+      return false;
+    }
+    return true;
+  }
+}
