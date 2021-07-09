079a7f0a6ddc71c48c158359e2295cc875b848dc
Ivo Zivkov <izivkov@gmail.com>
2021-07-08 23:02:44 -0400

    Fix style

--- android/app/src/main/java/org/openbot/robot/FreeRoamFragment.java
+++ android/app/src/main/java/org/openbot/robot/FreeRoamFragment.java
@@ -7,7 +7,6 @@ import static org.openbot.utils.Enums.SpeedMode;
 import android.annotation.SuppressLint;
 import android.content.res.ColorStateList;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
