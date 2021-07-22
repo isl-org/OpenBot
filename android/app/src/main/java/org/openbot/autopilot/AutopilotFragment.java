97d7edd2f937d0a9e10979eca8ceea935c898d76
MohammedZ666 <abuobaidazishan@gmail.com>
2021-07-22 10:53:19 +0600

    Updating controllInfo textView from the main thread

--- android/app/src/main/java/org/openbot/autopilot/AutopilotFragment.java
+++ android/app/src/main/java/org/openbot/autopilot/AutopilotFragment.java
@@ -440,8 +440,11 @@ public class AutopilotFragment extends CameraFragment implements ServerListener 
     vehicle.setControl(control);
     float left = vehicle.getLeftSpeed();
     float right = vehicle.getRightSpeed();
-    binding.controllerContainer.controlInfo.setText(
-        String.format(Locale.US, "%.0f,%.0f", left, right));
+    requireActivity()
+        .runOnUiThread(
+            () ->
+                binding.controllerContainer.controlInfo.setText(
+                    String.format(Locale.US, "%.0f,%.0f", left, right)));
   }
 
   @Override
