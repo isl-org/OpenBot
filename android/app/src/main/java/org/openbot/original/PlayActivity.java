fdf866b0692556fb627dc7a19308d9110aa73527
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2021-11-07 16:10:45 +0900

    app update

--- android/app/src/main/java/org/openbot/original/PlayActivity.java
+++ android/app/src/main/java/org/openbot/original/PlayActivity.java
@@ -3,74 +3,154 @@ package org.openbot.original;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.view.ViewTreeObserver;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.openbot.R;
 import org.openbot.env.SharedPreferencesManager;
 
-public class PlayActivity extends DefaultActivity {
-    private LinearLayout bottomSheetLayout;
-    private BottomSheetBehavior sheetBehavior;
-    protected ImageView bottomSheetArrowImageView;
-    private LinearLayout gestureLayout;
-    private SharedPreferencesManager preferencesManager;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 
-    @Override
+public class PlayActivity extends AppCompatActivity {
+
+    final Bundle bundle = new Bundle();
+    ArrayList x_list = new ArrayList();
+    ArrayList y_list = new ArrayList();
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
+        init();
 
-        bottomSheetLayout = findViewById(R.id.bottom_sheet_layout);
-        bottomSheetArrowImageView = findViewById(R.id.bottom_sheet_arrow);
-        sheetBehavior = BottomSheetBehavior.from(bottomSheetLayout);
-        gestureLayout = findViewById(R.id.gesture_layout);
-        preferencesManager = new SharedPreferencesManager(this);
 
-        ViewTreeObserver vto = gestureLayout.getViewTreeObserver();
-        vto.addOnGlobalLayoutListener(
-        new ViewTreeObserver.OnGlobalLayoutListener() {
+    }
+
+    private void init() {
+        EditText gx_num = findViewById(R.id.gx);
+        EditText gy_num = findViewById(R.id.gy);
+        Button sendBtn = findViewById(R.id.SendBtn);
+
+
+        sendBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onGlobalLayout() {
-                gestureLayout.getViewTreeObserver().removeOnGlobalLayoutListener(this);
-                // int width = bottomSheetLayout.getMeasuredWidth();
-                int height = gestureLayout.getMeasuredHeight();
+            public void onClick(View view) {
 
-                sheetBehavior.setPeekHeight(height);
+                x_list.clear();
+                y_list.clear();
+
+
+                String str = gx_num.getText().toString() + "/" + gy_num.getText().toString();
+                String url = "https://mysterious-sea-88696.herokuapp.com/" + str;
+
+                //Toast.makeText(getApplicationContext(), url, Toast.LENGTH_SHORT).show();
+
+
+                //Toast.makeText(getApplicationContext(), title, Toast.LENGTH_SHORT).show();
+
+
+                Handler handler = new Handler() {
+                    @Override
+                    public void handleMessage(@NonNull Message msg) {
+                        Bundle bundle = msg.getData();
+                        String temp = bundle.getString("hi");
+                        temp.trim();
+                        temp = temp.replace(" ", "");
+                        temp = temp.replace("[", "");
+                        temp = temp.replace("]", "");
+
+                        String[] arr = temp.split(",");
+
+
+                        for (int i = 0; i < arr.length; i++) {
+                            if (i % 2 == 0) {
+                                x_list.add(arr[i]);
+                            } else {
+                                y_list.add(arr[i]);
+                            }
+                        }
+
+
+                        Collections.reverse(x_list);
+                        Collections.reverse(y_list);
+
+
+                        System.out.println("사이즈는 " + x_list.size());
+                        System.out.println("사이즈는 " + y_list.size());
+
+
+
+                        System.out.println("x임다");
+
+                        for (int i = 0; i < x_list.size(); i++) {
+                            System.out.println(x_list.get(i));
+                        }
+
+                        System.out.println("y임다");
+
+                        for (int i = 0; i < y_list.size(); i++) {
+                            System.out.println(y_list.get(i));
+                        }
+
+
+                        Toast.makeText(getApplicationContext(), temp, Toast.LENGTH_SHORT).show();
+
+                    }
+                };
+
+
+                new Thread() {
+                    @Override
+                    public void run() {
+                        String title = "";
+
+
+                        Document doc = null;
+                        try {
+                            doc = Jsoup.connect(url).get();
+                            Elements mElementDatas = doc.select("body");
+                            title = mElementDatas.text();
+//                            for(Element elem : mElementDatas){
+//                                title = elem.select("body").text();
+//                            }
+                            //System.out.print(title);
+                            bundle.putString("hi", title);
+                            Message msg = handler.obtainMessage();
+                            msg.setData(bundle);
+                            handler.sendMessage(msg);
+
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                        super.run();
+                    }
+                }.start();
+
+
             }
         });
-        sheetBehavior.setHideable(false);
 
-        sheetBehavior.setBottomSheetCallback(
-                new BottomSheetBehavior.BottomSheetCallback() {
-                    @Override
-                    public void onStateChanged(@NonNull View bottomSheet, int newState) {
-                        switch (newState) {
-                            case BottomSheetBehavior.STATE_HIDDEN:
-                                break;
-                            case BottomSheetBehavior.STATE_EXPANDED:
-                                bottomSheetArrowImageView.setImageResource(R.drawable.icn_chevron_down);
-                                preferencesManager.setSheetExpanded(true);
-                                break;
-                            case BottomSheetBehavior.STATE_COLLAPSED:
-                                bottomSheetArrowImageView.setImageResource(R.drawable.icn_chevron_up);
-                                preferencesManager.setSheetExpanded(false);
-                                break;
-                            case BottomSheetBehavior.STATE_DRAGGING:
-                                break;
-                            case BottomSheetBehavior.STATE_SETTLING:
-                                bottomSheetArrowImageView.setImageResource(R.drawable.icn_chevron_up);
-                                break;
-                        }
-                    }
 
-                    @Override
-                    public void onSlide(@NonNull View bottomSheet, float slideOffset) {}
-                });
     }
 }
+
