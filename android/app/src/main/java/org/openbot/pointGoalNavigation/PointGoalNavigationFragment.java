30566e9badcd10039126be2701ec60bcd2fc9201
dvdhfnr <46796940+dvdhfnr@users.noreply.github.com>
2022-01-24 16:46:32 +0100

    start

--- android/app/src/main/java/org/openbot/pointGoalNavigation/PointGoalNavigationFragment.java
+++ android/app/src/main/java/org/openbot/pointGoalNavigation/PointGoalNavigationFragment.java
@@ -6,9 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import org.openbot.R;
-import org.openbot.common.ControlsFragment;
 
-public class PointGoalNavigationFragment extends ControlsFragment {
+public class PointGoalNavigationFragment extends Fragment {
 
   public PointGoalNavigationFragment() {
     // Required empty public constructor
@@ -33,15 +32,5 @@ public class PointGoalNavigationFragment extends ControlsFragment {
       Bundle savedInstanceState) {
     // Inflate the layout for this fragment
     return inflater.inflate(R.layout.fragment_point_goal_navigation, container, false);
-  }
-
-  @Override
-  protected void processControllerKeyData(String command) {
-
-  }
-
-  @Override
-  protected void processUSBData(String data) {
-
   }
 }
