6f39bcbc2b1d1facab0c9aeaf1403136667dd6e7
Matthias Mueller <thias15@web.de>
2022-02-05 17:01:49 +0100

    fix test

--- android/app/src/test/java/org/openbot/env/VehicleTest.java
+++ android/app/src/test/java/org/openbot/env/VehicleTest.java
@@ -23,33 +23,33 @@ public class VehicleTest {
 
   @Test
   public void getRotation() {
-    assertEquals(0, vehicle.getRotation(), 0.0);
+    assertEquals(0, vehicle.getRotation(), 0.01f);
 
-    vehicle.setControl(new Control(0.5f, 1));
-    assertEquals(-60, vehicle.getRotation(), 0.0);
+    vehicle.setControl(new Control(0.5f, 1.0f));
+    assertEquals(-60, vehicle.getRotation(), 0.01f);
 
-    vehicle.setControl(new Control(0f, 1));
-    assertEquals(-180, vehicle.getRotation(), 0.0);
+    vehicle.setControl(new Control(0.0f, 1.0f));
+    assertEquals(-180, vehicle.getRotation(), 0.01f);
   }
 
   @Test
   public void getSpeed() {
     vehicle.setSpeedFactor(Enums.SpeedMode.SLOW.getValue());
-    vehicle.setControl(new Control(-1, 1));
+    vehicle.setControl(new Control(-1.0f, 1.0f));
 
-    assertEquals(-0.25f, vehicle.getLeftSpeed(), 0.001f);
-    assertEquals(0.25f, vehicle.getRightSpeed(), 0.001f);
+    assertEquals(-0.25f, vehicle.getLeftSpeed(), 0.01f);
+    assertEquals(0.25f, vehicle.getRightSpeed(), 0.01f);
 
     vehicle.setSpeedFactor(Enums.SpeedMode.NORMAL.getValue());
-    vehicle.setControl(new Control(1, -1));
+    vehicle.setControl(new Control(1.0f, -1.0f));
 
-    assertEquals(0.5f, vehicle.getLeftSpeed(), 0.001f);
-    assertEquals(-0.5f, vehicle.getRightSpeed(), 0.001f);
+    assertEquals(0.5f, vehicle.getLeftSpeed(), 0.01f);
+    assertEquals(-0.5f, vehicle.getRightSpeed(), 0.01f);
 
     vehicle.setSpeedFactor(Enums.SpeedMode.FAST.getValue());
-    vehicle.setControl(new Control(1, 1));
+    vehicle.setControl(new Control(1.0f, 1.0f));
 
-    assertEquals(1.0f, vehicle.getLeftSpeed(), 0.001f);
-    assertEquals(1.0f, vehicle.getRightSpeed(), 0.001f);
+    assertEquals(1.0f, vehicle.getLeftSpeed(), 0.01f);
+    assertEquals(1.0f, vehicle.getRightSpeed(), 0.01f);
   }
 }
