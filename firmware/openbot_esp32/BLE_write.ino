945052bd035fbd76a46a8c0b58bb340f628b702e
nitishyadav <nitish@itinker.io>
2022-08-02 10:26:28 +0530

    adding firmware openbot_esp32 for ble

---
+++ firmware/openbot_esp32/BLE_write.ino
@@ -0,0 +1,44 @@
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "00000000-0000-0000-0000-000000000000"
+
+
+class MyCallbacks: public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic *pCharacteristic) {
+      std::string value = pCharacteristic->getValue();
+      if (value.length() > 0) {
+        Serial.println(value.c_str());
+      }
+    }
+};
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Connect to OpenBot::");
+  BLEDevice::init("OpenBot");
+  BLEServer *pServer = BLEDevice::createServer();
+
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+                                         CHARACTERISTIC_UUID,
+                                         BLECharacteristic::PROPERTY_READ |
+                                         BLECharacteristic::PROPERTY_WRITE
+                                       );
+
+  pCharacteristic->setCallbacks(new MyCallbacks());
+
+  pCharacteristic->setValue("OpenBot Serial");
+  pService->start();
+
+  BLEAdvertising *pAdvertising = pServer->getAdvertising();
+  pAdvertising->start();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(2000);
+}
