3e3c66b997e519d3843806309004f542bc3eba82
Sparsh Jain <sparsh@itinker.io>
2022-08-18 11:52:08 +0530

    updating ui for the home grid

--- ios/OpenBot/OpenBot/AllModesCollectionView.swift
+++ ios/OpenBot/OpenBot/AllModesCollectionView.swift
@@ -8,33 +8,41 @@
 import UIKit
 
 class AllModesCollectionView: UICollectionView, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-    private var labels = ["Free Roam", "Data Collection", "Controller Mapping"]
+    struct gridItem {
+        var label: String;
+        var icon: UIImage?;
+    }
+
+    private var gridItems = [gridItem(label: "Free Roam", icon: UIImage(named: "freeRoam")), gridItem(label: "Data Collection", icon: UIImage(named: "dataCollection")), gridItem(label: "Controller Mapping", icon: UIImage(named: "controllerMapping"))];
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath as IndexPath) as! AllModesCollectionViewCell
 
-        cell.setLabel(label: labels[indexPath.row])
+        cell.setContent(label: gridItems[indexPath.row].label, image: gridItems[indexPath.row].icon!);
         return cell
     }
 
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        self.dataSource = self
-        self.delegate = self
+        dataSource = self
+        delegate = self
     }
 
     func collectionView(_ collectionView: UICollectionView,
                         numberOfItemsInSection section: Int) -> Int {
-        return labels.count
+        gridItems.count
     }
 
     func collectionView(_ collectionView: UICollectionView, numberOfSections section: Int) -> Int {
-        return 2
+        2
     }
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 150, height: 150)
+        CGSize(width: 150, height: 150)
     }
 
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+
+    }
 
 }
--- ios/OpenBot/OpenBot/AllModesCollectionViewCell.swift
+++ ios/OpenBot/OpenBot/AllModesCollectionViewCell.swift
@@ -9,8 +9,19 @@ import UIKit
 
 class AllModesCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var icon: UIImageView!
+    @IBOutlet weak var content: UIView!
 
-    func setLabel(label: String) {
+    func setContent(label: String, image: UIImage) {
         title.text = label
+        icon.image = image;
+    }
+
+    override func layoutSubviews() {
+        content.layer.cornerRadius = 10
+        content.layer.shadowColor = UIColor(named: "gridItemShadowColor")?.cgColor
+        content.layer.shadowOpacity = 1
+        content.layer.shadowOffset = CGSize(width: 0, height: 4)
+        content.layer.shadowRadius = 14 / 2
     }
 }
