5d899a68d1b2e7d62b4fc5c23b26ef1002b9b6ba
Sanjeev Sharma <>
2022-11-18 17:31:40 +0530

    getting available ble devices + connecting to specific device + retrieving data in bytes

---
+++ android/app/src/main/java/org/openbot/main/ByteUtils.java
@@ -0,0 +1,29 @@
+package org.openbot.main;
+
+public class ByteUtils {
+    public static String bytes2HexStr(byte[] bytes) {
+        if (bytes == null) {
+            return null;
+        }
+        StringBuilder b = new StringBuilder();
+        for (int i = 0; i < bytes.length; i++) {
+            b.append(String.format("%02x", bytes[i] & 0xFF));
+        }
+        return b.toString();
+    }
+
+    public static byte[] hexStr2Bytes(String str) {
+        if (str == null) {
+            return null;
+        }
+        if (str.length() == 0) {
+            return new byte[0];
+        }
+        byte[] byteArray = new byte[str.length() / 2];
+        for (int i = 0; i < byteArray.length; i++) {
+            String subStr = str.substring(2 * i, 2 * i + 2);
+            byteArray[i] = ((byte) Integer.parseInt(subStr, 16));
+        }
+        return byteArray;
+    }
+}
