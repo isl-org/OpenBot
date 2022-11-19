d1ea31e3b99d6fc0babba70a1dc1452f5229f5ba
nitishpratap18 <nitish@itinker.io>
2022-11-19 17:09:59 +0530

    created model management

---
+++ ios/OpenBot/OpenBot/Controllers/Model Management/ModelManagementFragment.swift
@@ -0,0 +1,305 @@
+//
+// Created by Nitish Yadav on 18/11/22.
+//
+
+import Foundation
+import UIKit
+import DropDown
+
+class ModelManagementFragment: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    var header = UIView()
+    var modelTable = UITableView()
+    var widthOfTable: NSLayoutConstraint!;
+    var heightOfTable: NSLayoutConstraint!
+    var topAnchorOfTable: NSLayoutConstraint!;
+    var leadingAnchorOFTable: NSLayoutConstraint!;
+    var widthOfHeader: NSLayoutConstraint!
+    var heightOfHeader: NSLayoutConstraint!
+    var leadingAnchorOfDropdown: NSLayoutConstraint!
+    var widthOfDropDownView: NSLayoutConstraint!
+    var modelDropdown = DropDown()
+    var modelClassLabel = UILabel()
+    var models: [String]!
+    var popupWindow = UIView();
+    var blankScreen = UIView()
+    var blankScreenWidth: NSLayoutConstraint!
+    var blankScreenHeight: NSLayoutConstraint!
+    var blankScreenLeadingAnchor: NSLayoutConstraint!
+    var blankScreenTopAnchor: NSLayoutConstraint!
+    var popupWindowWidth: NSLayoutConstraint!
+    var popupWindowLeadingAnchor: NSLayoutConstraint!
+    var popupWindowTopAnchor: NSLayoutConstraint!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTable()
+        createHeader()
+        setupNavigationBarItem()
+        setupConfiguration()
+        createDropdownSelector()
+    }
+
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        updateModelItemList(type: "All")
+    }
+
+    func createHeader() {
+        view.addSubview(header);
+        setupHeader()
+    }
+
+    func setupHeader() {
+        header.translatesAutoresizingMaskIntoConstraints = false;
+        header.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true;
+        header.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10).isActive = true
+        heightOfHeader = header.heightAnchor.constraint(equalToConstant: 100);
+        createModelHeading()
+        modelDropdown.hide()
+        createModelSelectorDropDown()
+
+    }
+
+    func createModelHeading() {
+        let modelLabel = createLabel(text: "Model", leadingAnchor: 10, topAnchor: 50, isBoldNeede: true);
+        header.addSubview(modelLabel)
+        modelLabel.translatesAutoresizingMaskIntoConstraints = false;
+        modelLabel.topAnchor.constraint(equalTo: header.topAnchor, constant: 30).isActive = true;
+        modelLabel.leadingAnchor.constraint(equalTo: header.leadingAnchor, constant: 10).isActive = true;
+        modelLabel.font = UIFont.boldSystemFont(ofSize: 25.0)
+    }
+
+
+    func setupTable() {
+        modelTable.translatesAutoresizingMaskIntoConstraints = false
+        topAnchorOfTable = modelTable.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100)
+        leadingConstraint = modelTable.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0)
+        view.addSubview(modelTable)
+        modelTable.dataSource = self
+        modelTable.delegate = self
+        modelTable.register(CustomTableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+
+    func setupNavigationBarItem() {
+        if UIImage(named: "back") != nil {
+            let backNavigationIcon = (UIImage(named: "back")?.withRenderingMode(.alwaysOriginal))!
+            let newBackButton = UIBarButtonItem(image: backNavigationIcon, title: Strings.modelManagement, target: self, action: #selector(ModelManagementFragment.back(sender:)))
+            navigationItem.leftBarButtonItem = newBackButton
+        }
+    }
+
+    func setupConfiguration() {
+        if currentOrientation == .portrait {
+            widthOfTable = modelTable.widthAnchor.constraint(equalToConstant: width)
+            heightOfTable = modelTable.heightAnchor.constraint(equalToConstant: height)
+            widthOfHeader = header.widthAnchor.constraint(equalToConstant: width);
+        } else {
+            widthOfHeader = header.widthAnchor.constraint(equalToConstant: height);
+            widthOfTable = modelTable.widthAnchor.constraint(equalToConstant: height)
+            heightOfTable = modelTable.heightAnchor.constraint(equalToConstant: width)
+        }
+        NSLayoutConstraint.activate([topAnchorOfTable, leadingConstraint, widthOfTable, heightOfTable]);
+        NSLayoutConstraint.activate([widthOfHeader, heightOfHeader]);
+    }
+
+    func createModelSelectorDropDown() {
+        modelDropdown.backgroundColor = Colors.freeRoamButtonsColor;
+        modelDropdown.textColor = Colors.borderColor!
+        let dropDownView = UIView(frame: CGRect(x: width / 2 + 20, y: 100, width: width / 2 - 80, height: 200));
+        view.addSubview(dropDownView);
+        modelDropdown.dataSource = ["All", "AUTOPilot", "Detector", "Navigation"];
+        modelDropdown.anchorView = dropDownView
+        modelDropdown.width = 100;
+        modelDropdown.selectionAction = { [unowned self] (index: Int, item: String) in
+            modelClassLabel.text = item
+            updateTable()
+            updateModelItemList(type: item)
+        }
+    }
+
+    func createDropdownSelector() {
+        let ddView = UIView();
+        view.addSubview(ddView);
+        ddView.translatesAutoresizingMaskIntoConstraints = false
+        ddView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 25).isActive = true;
+        if currentOrientation == .portrait {
+            leadingAnchorOfDropdown = ddView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: width / 2)
+            widthOfDropDownView = ddView.widthAnchor.constraint(equalToConstant: width / 2 - 30)
+        } else {
+            leadingAnchorOfDropdown = ddView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: height / 2)
+            widthOfDropDownView = ddView.widthAnchor.constraint(equalToConstant: height / 2 - 30)
+        }
+        widthOfDropDownView.isActive = true
+        leadingAnchorOfDropdown.isActive = true;
+        ddView.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(showModelDropdown(_:)))
+        ddView.addGestureRecognizer(tapGesture)
+        modelClassLabel = createLabel(text: "All", leadingAnchor: 1, topAnchor: 1, isBoldNeede: false);
+        ddView.addSubview(modelClassLabel);
+        modelClassLabel.translatesAutoresizingMaskIntoConstraints = false;
+        modelClassLabel.leadingAnchor.constraint(equalTo: ddView.leadingAnchor, constant: 0).isActive = true;
+        modelClassLabel.topAnchor.constraint(equalTo: ddView.topAnchor, constant: 0).isActive = true;
+        modelClassLabel.widthAnchor.constraint(equalToConstant: 100).isActive = true;
+        modelClassLabel.heightAnchor.constraint(equalToConstant: 40).isActive = true;
+        let downwardImage = UIImageView()
+        downwardImage.frame.size = CGSize(width: 5, height: 5)
+        downwardImage.image = Images.downArrow
+        ddView.addSubview(downwardImage)
+        downwardImage.tintColor = Colors.borderColor
+        downwardImage.translatesAutoresizingMaskIntoConstraints = false
+        downwardImage.trailingAnchor.constraint(equalTo: ddView.trailingAnchor, constant: -10).isActive = true
+        downwardImage.topAnchor.constraint(equalTo: ddView.topAnchor, constant: 11.5).isActive = true
+    }
+
+    func updateConfiguration() {
+        if currentOrientation == .portrait {
+            widthOfHeader.constant = width;
+            widthOfTable.constant = width;
+            leadingAnchorOfDropdown.constant = width / 2;
+            widthOfDropDownView.constant = width / 2 - 30
+            if blankScreenWidth != nil {
+                blankScreenWidth.constant = width
+                blankScreenHeight.constant = height
+                popupWindowWidth.constant = width - 20
+                popupWindowTopAnchor.constant = 100;
+                popupWindowLeadingAnchor.constant = 10
+            }
+
+
+        } else {
+            widthOfHeader.constant = height;
+            widthOfTable.constant = height;
+            leadingAnchorOfDropdown.constant = height / 2;
+            widthOfDropDownView.constant = height / 2 - 30
+            if blankScreenWidth != nil {
+                blankScreenWidth.constant = height
+                blankScreenHeight.constant = width
+                popupWindowWidth.constant = height - 50
+                popupWindowTopAnchor.constant = 10;
+                popupWindowLeadingAnchor.constant = 25
+            }
+
+        }
+    }
+
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        updateConfiguration()
+    }
+
+    @objc func back(sender: UIBarButtonItem) {
+        _ = navigationController?.popViewController(animated: true)
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        models.count
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        60
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = modelTable.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CustomTableViewCell
+        cell.imgUser.image = UIImage(named: "download-cloud")
+        cell.textLabel?.text = models[indexPath.item]
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        popupWindow = popupWindowView()
+        createBlankScreen()
+        createPopupWindow()
+    }
+
+    func createLabel(text: String, leadingAnchor: Int, topAnchor: Int, isBoldNeede: Bool) -> UILabel {
+        let label = UILabel()
+        label.text = text;
+        label.textColor = Colors.borderColor;
+        return label;
+
+    }
+
+    func createBlankScreen() {
+        view.addSubview(blankScreen);
+        blankScreen.backgroundColor = Colors.freeRoamButtonsColor;
+        blankScreen.translatesAutoresizingMaskIntoConstraints = false
+        if currentOrientation == .portrait {
+            blankScreenWidth = blankScreen.widthAnchor.constraint(equalToConstant: width);
+            blankScreenHeight = blankScreen.heightAnchor.constraint(equalToConstant: height);
+        } else {
+            blankScreenWidth = blankScreen.widthAnchor.constraint(equalToConstant: height);
+            blankScreenHeight = blankScreen.heightAnchor.constraint(equalToConstant: width);
+        }
+        NSLayoutConstraint.activate([blankScreenWidth, blankScreenHeight]);
+    }
+
+    func createPopupWindow() {
+        view.addSubview(popupWindow)
+        popupWindow.translatesAutoresizingMaskIntoConstraints = false
+        popupWindow.heightAnchor.constraint(equalToConstant: 400).isActive = true;
+        popupWindowLeadingAnchor = popupWindow.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10)
+        popupWindowTopAnchor = popupWindow.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100)
+        if currentOrientation == .portrait {
+            popupWindowWidth = popupWindow.widthAnchor.constraint(equalToConstant: width - 20)
+            popupWindowLeadingAnchor = popupWindow.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10)
+            popupWindowTopAnchor = popupWindow.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100)
+        } else {
+            popupWindowWidth = popupWindow.widthAnchor.constraint(equalToConstant: height - 50);
+            popupWindowLeadingAnchor = popupWindow.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 25)
+            popupWindowTopAnchor = popupWindow.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10)
+        }
+        NSLayoutConstraint.activate([popupWindowWidth, popupWindowTopAnchor, popupWindowLeadingAnchor])
+        popupWindow.backgroundColor = .black
+    }
+
+    @objc func showModelDropdown(_ sender: UIButton) {
+        modelDropdown.show()
+    }
+
+    func updateModelItemList(type: String) {
+        switch type {
+        case "All":
+            models = Common.loadAllModels()
+        case "AUTOPilot":
+            models = Common.loadAllSelectedModelItems(mode: "AUTOPILOT")
+        case "Detector":
+            models = Common.loadAllSelectedModelItems(mode: "DETECTOR")
+        case "Navigation":
+            models = []
+        default:
+            models = Common.loadAllModels()
+        }
+        updateTable()
+    }
+
+    func updateTable() {
+        modelTable.reloadData()
+    }
+}
+
+class CustomTableViewCell: UITableViewCell {
+
+    let imgUser = UIImageView()
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.addSubview(imgUser)
+        imgUser.widthAnchor.constraint(equalToConstant: 40).isActive = true;
+        imgUser.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        imgUser.translatesAutoresizingMaskIntoConstraints = false
+        imgUser.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -25).isActive = true
+        imgUser.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10).isActive = true
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
+
+
---
+++ ios/OpenBot/OpenBot/Controllers/Model Management/popupWindowView.swift
@@ -0,0 +1,17 @@
+//
+// Created by Nitish Yadav on 19/11/22.
+//
+
+import Foundation
+import UIKit
+class popupWindowView: UIView {
+
+
+         override init(frame : CGRect) {
+              super.init(frame: frame)
+           }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
