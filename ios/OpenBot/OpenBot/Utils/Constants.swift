2d1c83391dc30762c20547d0fb30542f753c52c0
nitishpratap18 <nitish@itinker.io>
2022-12-08 15:23:27 +0530

    Removed GPU from the devices list for now

--- ios/OpenBot/OpenBot/Utils/Constants.swift
+++ ios/OpenBot/OpenBot/Utils/Constants.swift
@@ -65,7 +65,7 @@ struct Constants {
     static let objectTrackingMode = "DETECTOR";
     static let types: [String] = ["AUTOPILOT", "DETECTOR", "NAVIGATION"];
     static let classes: [String] = ["AUTOPILOT_F", "MOBILENETV1_1_0_Q", "MOBILENETV3_S_Q", "YOLOV4", "NAVIGATION"]
-    static let devices : [String] = ["CPU","GPU","XNNPACK"]
+    static let devices : [String] = ["CPU","XNNPACK"]
 }
 
 enum SpeedMode: Float {
