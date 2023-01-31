7bbc06885d7bc9a43472d5ee4af91a7d368edd41
Sanjeev <>
2023-01-31 18:02:24 +0530

    set function to blink indicator

--- android/app/src/main/java/org/openbot/convertJStoJAVA/convertJStoJAVAfragment.java
+++ android/app/src/main/java/org/openbot/convertJStoJAVA/convertJStoJAVAfragment.java
@@ -34,54 +34,94 @@ public class convertJStoJAVAfragment extends Fragment {
             @Override
             public void onClick(View v) {
                 evaluate();
+//                qrScanResult();
             }
         });
 
     }
 
+    public String qrScanResult(){
+        String cmdObject = "let cmdObject = []; function blinkLeft() {cmdObject.push({\"INDICATOR_LEFT\" : \"true\"});}";
+        String returncmdObject = "return JSON.stringify(cmdObject)";
+        String code = "function openCode() { var name = 1; while (name <= 5){ blinkLeft(); name = name + 1; }}";
+        String newString = new String();
+
+        for (int i = 0; i < code.length(); i++) {
+            // Insert the original string character
+            // into the new string
+            newString += code.charAt(i);
+            if (i == 20) {
+                // Insert the string to be inserted
+                // into the new string
+                newString += cmdObject;
+            }
+            if (i == code.length()-2){
+                newString += returncmdObject;
+            }
+        }
+//        System.out.println("value = "+newString);
+        return newString;
+    }
+
     private void evaluate() {
         JsEvaluator jsEvaluator = new JsEvaluator(this.getActivity());
+        System.out.println("value = 1 " +qrScanResult());
 
-        jsEvaluator.evaluate("2 * 20", new JsCallback() {
-            @Override
-            public void onResult(String result) {
-                System.out.println("result = " + result);
-                // Process result here.
-                // This method is called in the UI thread.
-            }
+//        jsEvaluator.evaluate("2 * 20", new JsCallback() {
+//            @Override
+//            public void onResult(String result) {
+//                System.out.println("result = " + result);
+//                // Process result here.
+//                // This method is called in the UI thread.
+//            }
+//
+//            @Override
+//            public void onError(String errorMessage) {
+//                System.out.println("errorMessage = " + errorMessage);
+//
+//                // Process JavaScript error here.
+//                // This method is called in the UI thread.
+//            }
+//        });
 
-            @Override
-            public void onError(String errorMessage) {
-                System.out.println("errorMessage = " + errorMessage);
 
-                // Process JavaScript error here.
-                // This method is called in the UI thread.
-            }
-        });
-
-        jsEvaluator.callFunction("function greet(name) { return \"hello, \" + name; }", new JsCallback() {
+//        jsEvaluator.callFunction("function greet(name) { return \"hello, \" + name; }", new JsCallback() {
+//            @Override
+//            public void onResult(String value) {
+//                System.out.println(value);
+//            }
+//
+//            @Override
+//            public void onError(String errorMessage) {
+//                System.out.println("error = " + errorMessage);
+//            }
+//        }, "greet", "something");
+//
+//        jsEvaluator.callFunction("function greet() { var name = 1; while (name <= 3){ console.log(\"sanjeev\"); name = name + 1} return name;}", new JsCallback() {
+//            @Override
+//            public void onResult(String value) {
+//                System.out.println(value);
+//            }
+//
+//            @Override
+//            public void onError(String errorMessage) {
+//                System.out.println("error = " + errorMessage);
+//            }
+//        }, "greet");
+        jsEvaluator.callFunction(qrScanResult(), new JsCallback() {
             @Override
             public void onResult(String value) {
-                System.out.println(value);
+
+                System.out.println("value = 2 " +value);
             }
 
             @Override
             public void onError(String errorMessage) {
-                System.out.println("error = " + errorMessage);
-            }
-        }, "greet", "something");
+                System.out.println("value = e " +errorMessage);
 
-        jsEvaluator.callFunction("function greet() { var name = 1; while (name <= 3){ console.log(\"sanjeev\"); name = name + 1} return name;}", new JsCallback() {
-            @Override
-            public void onResult(String value) {
-                System.out.println(value);
-            }
 
-            @Override
-            public void onError(String errorMessage) {
-                System.out.println("error = " + errorMessage);
             }
-        }, "greet");
+        }, "openCode");
     }
 
 }
