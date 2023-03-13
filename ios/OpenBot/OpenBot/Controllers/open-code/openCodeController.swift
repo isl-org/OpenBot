5e4e5ff84bbfc3b9c360d47648c6315afdeff7dc
nitishpratap18 <nitish@itinker.io>
2023-03-13 15:22:50 +0530

    added qr scanner and start and loop functions

--- ios/OpenBot/OpenBot/Controllers/open-code/openCodeController.swift
+++ ios/OpenBot/OpenBot/Controllers/open-code/openCodeController.swift
@@ -5,18 +5,62 @@
 import Foundation
 import UIKit
 import JavaScriptCore
-
-class openCodeController: UIViewController {
+import AVFoundation
+class openCodeController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
     var jsContext: JSContext!
-
+    let qrScanner  = UIImageView()
+    let captureSession = AVCaptureSession()
+    let cameraView = UIView();
     override func viewDidLoad() {
         super.viewDidLoad();
+        initializeQrScannerIcon();
         initializeJS();
-        let script = "function secondFunction() { return 'i am Second Function'; } function getString(arg) { return 'Hello ' + arg + '! ' + secondFunction() ;}"
-        jsContext?.evaluateScript(script)
-        jsContext?.evaluateScript(script)
+        cameraView.frame = CGRect(x: 0, y: 100, width: width, height: 300);
+        view.addSubview(cameraView);
+        initializeCamera();
+        createQRScanner();
         evaluateJavaScript()
 
+    }
+
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        for metadataObject in metadataObjects {
+            guard let qrCodeObject = metadataObject as? AVMetadataMachineReadableCodeObject,
+                  qrCodeObject.type == AVMetadataObject.ObjectType.qr else {
+                continue
+            }
+            if let qrCodeString = qrCodeObject.stringValue {
+                // Handle the QR code data here
+                print("QR code data: \(qrCodeString)")
+                captureSession.stopRunning();
+                print("Qr Scan Successful");
+            }
+        }
+    }
+
+
+
+
+    func initializeCamera(){
+        // Configure the input device (camera)
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else { return }
+        captureSession.addInput(input);
+// Configure the output device (metadata)
+        let output = AVCaptureMetadataOutput();
+        captureSession.addOutput(output);
+        output.setMetadataObjectsDelegate(self, queue: DispatchQueue.main);
+        output.metadataObjectTypes = [.qr] // specify the types of metadata to capture (in this case, only QR codes)
+
+    }
+
+    func initializeQrScannerIcon(){
+        qrScanner.image = UIImage(systemName: "qrcode.viewfinder");
+        qrScanner.frame = CGRect(x: width/2, y: height/2, width: 100, height: 100);
+        view.addSubview(qrScanner)
+        qrScanner.isUserInteractionEnabled = true;
+        let tap = UITapGestureRecognizer(target: self, action: #selector(runQrScan(_:)))
+        qrScanner.addGestureRecognizer(tap);
     }
 
     func initializeJS() {
@@ -27,20 +71,58 @@ class openCodeController: UIViewController {
         print("calling swift from js")
     }
 
+    func start(){
+        print("inside start");
+    }
+
+    func loop(){
+        print("inside loop")
+    }
+    func createQRScanner(){
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.videoGravity = .resizeAspectFill
+        previewLayer.frame = cameraView.layer.bounds
+        cameraView.layer.addSublayer(previewLayer)
+        captureSession.startRunning()
+    }
+
 
     func evaluateJavaScript() {
         if let context = JSContext() {
             let callSwiftFunction: @convention(block) () -> Void = { () in
                 return self.swiftFunction()
             }
+            let loop: @convention(block) () -> Void = { () in
+                return self.loop();
+            }
+            let start: @convention(block) () -> Void = { () in
+                return self.start();
+            }
             context.setObject(callSwiftFunction,
                     forKeyedSubscript: "swiftFunction" as NSString)
+            context.setObject(loop,
+                    forKeyedSubscript: "loop" as NSString)
+            context.setObject(loop,
+                    forKeyedSubscript: "start" as NSString)
             context.evaluateScript("""
-                                   for(let i = 0;i<10;i++)
-                                   {swiftFunction()
+                                   function swift(){
+                                   swiftFunction();
                                    }
+                                   function loopFunction(){
+                                   loop();
+                                   }
+                                   function startFunction(){
+                                   start();
+                                   }
+                                   swift();
+                                   loopFunction();
+                                   startFunction();
                                    """)
         }
 
+    }
+
+    @objc func runQrScan(_ sender: UIImageView){
+            print("Hello from tap")
     }
 }
