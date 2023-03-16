917670cf2364eb5f8f230bb25318209891f30539
Sanjeev <>
2023-03-16 12:33:21 +0530

    call java function from qr code dynamic.

---
+++ android/app/src/main/java/org/openbot/convertJStoJAVA/BarCodeScannerFragment.java
@@ -0,0 +1,108 @@
+package org.openbot.convertJStoJAVA;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.SparseArray;
+import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import org.openbot.R;
+import org.openbot.databinding.FragmentBarCodeScannerBinding;
+
+import java.io.IOException;
+
+import timber.log.Timber;
+
+
+public class BarCodeScannerFragment extends Fragment {
+
+    public static String barCodeValue;
+    private FragmentBarCodeScannerBinding binding;
+    private SurfaceView surfaceView;
+    private BarcodeDetector barcodeDetector;
+    private CameraSource cameraSource;
+    private static final int REQUEST_CAMERA_PERMISSION = 201;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentBarCodeScannerBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @NonNull Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        surfaceView = getView().findViewById(R.id.surface_view);
+        initialiseDetectorsAndSources();
+    }
+
+    private void initialiseDetectorsAndSources() {
+
+        barcodeDetector = new BarcodeDetector.Builder(getContext())
+                .setBarcodeFormats(Barcode.ALL_FORMATS)
+                .build();
+
+        cameraSource = new CameraSource.Builder(getContext(), barcodeDetector)
+                .setRequestedPreviewSize(1920, 1080)
+                .setAutoFocusEnabled(true) //you should add this feature
+                .build();
+
+        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                try {
+                    if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                        cameraSource.start(surfaceView.getHolder());
+                    } else {
+                        ActivityCompat.requestPermissions(getActivity(), new
+                                String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.stop();
+            }
+        });
+
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+            }
+
+            @Override
+            public void receiveDetections(@NonNull Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+                if (barcodes.size() != 0) {
+                    barCodeValue = barcodes.valueAt(0).displayValue;
+                    requireActivity().getOnBackPressedDispatcher().onBackPressed();
+                }
+            }
+        });
+    }
+}
--- android/app/src/main/java/org/openbot/convertJStoJAVA/BotFunction.java
+++ android/app/src/main/java/org/openbot/convertJStoJAVA/BotFunction.java
@@ -2,18 +2,17 @@ package org.openbot.convertJStoJAVA;
 
 import android.webkit.JavascriptInterface;
 
+import timber.log.Timber;
+
 public class BotFunction {
 
     @JavascriptInterface
-    public static void blinkLeft(int a) {
-        System.out.println("i am blinkLeft = " + a);
+    public static void moveCircular(int a) {
+        Timber.tag("Qr sanjeev").d("i am moveCircular = " + a);
     }
+
     @JavascriptInterface
     public static void moveForward(int a) {
-        System.out.println("i am moveForward = " + a);
-    }
-    @JavascriptInterface
-    public static void moveCircular(int a) {
-        System.out.println("i am moveCircular = " + a);
+        Timber.tag("Qr sanjeev").d("i am moveForward = " + a);
     }
 }
