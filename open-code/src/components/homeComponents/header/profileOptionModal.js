5f6ebdcc029a2dd884245aa43179e06d1114120e
Mansi Sisangiya <>
2023-04-01 17:14:25 +0530

    added profile edit option's mobile view in home screen

--- open-code/src/components/homeComponents/header/profileOptionModal.js
+++ open-code/src/components/homeComponents/header/profileOptionModal.js
@@ -1,12 +1,14 @@
 import React, {useContext} from "react";
 import {ThemeContext} from "../../../App";
-import {Box, Modal} from "@mui/material";
+import {Box, Modal, useTheme} from "@mui/material";
 import styles from "../../navBar/navbar.module.css";
 import {colors} from "../../../utils/color";
 import {Images} from "../../../utils/images";
 import WhiteText from "../../fonts/whiteText";
 import BlueText from "../../fonts/blueText";
-import {Themes} from "../../../utils/constants";
+import {PathName, Themes} from "../../../utils/constants";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import {useLocation} from "react-router-dom";
 
 export function ProfileOptionModal(props) {
     const {
@@ -16,6 +18,9 @@ export function ProfileOptionModal(props) {
         setIsLogoutModal,
     } = props
     const theme = useContext(ThemeContext);
+    const themes = useTheme();
+    const isMobile = useMediaQuery(themes.breakpoints.down('md'));
+    const location = useLocation();
 
     const handleClose = () => {
         setIsProfileModal(false)
@@ -34,28 +39,40 @@ export function ProfileOptionModal(props) {
             onClose={handleClose}>
             <Box className={styles.profileOptionModalStyle}
                  style={{backgroundColor: theme.theme === Themes.dark ? colors.blackPopupBackground : colors.whiteBackground}}>
-
-                <div onClick={handleEditProfile}
-                     className={`${styles.item} ${styles.editProfileMargin}  ${(theme.theme === Themes.dark ? styles.darkItem : styles.lightItem)}`}>
-
-                    <img alt="icon" src={theme.theme === Themes.dark ? Images.darkUserIcon : Images.userIcon}
-                         className={styles.modalIcon}/>
-                    {theme.theme === Themes.dark ?
-                        <WhiteText extraStyle={styles.modalText} styles text={"Edit Profile"}/> :
-                        <BlueText extraStyle={styles.modalText} styles text={"Edit Profile"}/>}
-                </div>
-
-                <div onClick={handleLogOut}
-                     className={`${styles.item} ${styles.logOutMargin} ${(theme.theme === Themes.dark ? styles.darkItem : styles.lightItem)}`}>
-
-                    <img alt="icon" src={theme.theme === Themes.dark ? Images.darkLogoutIcon : Images.logoutIcon}
-                         className={styles.modalIcon}/>
-                    {theme.theme === Themes.dark ?
-                        <WhiteText extraStyle={styles.modalText} text={"Logout"}/> :
-                        <BlueText extraStyle={styles.modalText} text={"Logout"}/>}
-                </div>
+                <PopUpInRowText onClick={handleEditProfile} text={"Edit Profile"}
+                              icon={theme.theme === Themes.dark ? Images.darkUserIcon : Images.userIcon}/>
+                <PopUpInRowText onClick={handleLogOut} text={"Logout"}
+                              icon={theme.theme === Themes.dark ? Images.darkLogoutIcon : Images.logoutIcon}/>
+                {/*{isMobile && location.pathname === PathName.playGround &&*/}
+                {/*    <PopUpInRowText onClick={handleLogOut} text={"Logout"}*/}
+                {/*    icon={theme.theme === Themes.dark ? Images.darkLogoutIcon : Images.logoutIcon}/>*/}
+                {/*}*/}
 
             </Box>
         </Modal>
+    )
+}
+
+
+/**
+ * display icon with text in popup
+ * @param params
+ * @returns {JSX.Element}
+ * @constructor
+ */
+function PopUpInRowText(params) {
+    const theme = useContext(ThemeContext);
+    const {onClick, text, icon} = params
+    return (
+        <div onClick={onClick}
+             className={`${styles.item} ${styles.editProfileMargin}  ${(theme.theme === Themes.dark ? styles.darkItem : styles.lightItem)}`}>
+            <img alt="icon" src={icon}
+                 className={styles.modalIcon}/>
+            <span className={`${styles.inRowText}`}>
+                {theme.theme === Themes.dark ?
+                    <WhiteText extraStyle={styles.modalText} styles text={text}/> :
+                    <BlueText extraStyle={styles.modalText} styles text={text}/>}
+            </span>
+        </div>
     )
 }
