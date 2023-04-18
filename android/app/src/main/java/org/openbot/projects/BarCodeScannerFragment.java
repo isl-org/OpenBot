0778913bd41c7f2fb65cd07ef5fb7013c7302add
Sanjeev <>
2023-04-18 16:01:37 +0530

    barcode fragmnet extend with camera fragment

--- android/app/src/main/java/org/openbot/projects/BarCodeScannerFragment.java
+++ android/app/src/main/java/org/openbot/projects/BarCodeScannerFragment.java
@@ -1,37 +1,29 @@
 package org.openbot.projects;
 
-import android.Manifest;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.SparseArray;
 import android.view.LayoutInflater;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
+import androidx.camera.core.ImageProxy;
 import androidx.navigation.Navigation;
-import com.google.android.gms.vision.CameraSource;
-import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.Frame;
 import com.google.android.gms.vision.barcode.Barcode;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import java.io.IOException;
 import org.openbot.R;
+import org.openbot.common.CameraFragment;
 import org.openbot.databinding.FragmentBarCodeScannerBinding;
 import org.openbot.utils.BotFunctionUtils;
 
-public class BarCodeScannerFragment extends Fragment {
+public class BarCodeScannerFragment extends CameraFragment {
 
-  private SurfaceView surfaceView;
-  private View overlayView;
+  private FragmentBarCodeScannerBinding binding;
   private BarcodeDetector barcodeDetector;
-  private CameraSource cameraSource;
   private BottomSheetBehavior successBottomSheetBehavior;
   private BottomSheetBehavior failedBottomSheetBehavior;
   private boolean barCodeAccess = true;
@@ -41,111 +33,72 @@ public class BarCodeScannerFragment extends Fragment {
   public View onCreateView(
       @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
     // Inflate the layout for Barcode Scanner View fragment.
-    FragmentBarCodeScannerBinding binding =
-        FragmentBarCodeScannerBinding.inflate(inflater, container, false);
+    binding = FragmentBarCodeScannerBinding.inflate(inflater, container, false);
 
     // initialise 2 bottom-sheet one is for correct QR code another one is for wrong QR code.
-    successBottomSheetBehavior =
-        BottomSheetBehavior.from(binding.getRoot().findViewById(R.id.qr_bottom_sheet_success));
-    failedBottomSheetBehavior =
-        BottomSheetBehavior.from(binding.getRoot().findViewById(R.id.qr_bottom_sheet_error));
+    successBottomSheetBehavior = BottomSheetBehavior.from(binding.qrBottomSheetSuccess);
+    failedBottomSheetBehavior = BottomSheetBehavior.from(binding.qrBottomSheetError);
     successBottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
     failedBottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
-    return binding.getRoot();
+    return inflateFragment(binding, inflater, container);
   }
 
   @Override
   public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
-    surfaceView = requireView().findViewById(R.id.surface_view);
     ImageView crossImageView = requireView().findViewById(R.id.btnCross);
-    LinearLayout qrCancelBtn = requireView().findViewById(R.id.qr_cancel_btn);
-    LinearLayout qrStartBtn = requireView().findViewById(R.id.qr_start_btn);
-    LinearLayout qrReScanBtn = requireView().findViewById(R.id.re_scan_btn);
-    overlayView = requireView().findViewById(R.id.overlay_view);
-    startScan();
     crossImageView.setOnClickListener(
         v -> Navigation.findNavController(requireView()).popBackStack());
-    qrCancelBtn.setOnClickListener(v -> onQRCancelButton());
-    qrStartBtn.setOnClickListener(v -> onQRStartButton());
-    qrReScanBtn.setOnClickListener(v -> onQRReScanButton());
+    binding.qrCancelBtn.setOnClickListener(v -> onQRCancelButton());
+    binding.qrStartBtn.setOnClickListener(v -> onQRStartButton());
+    binding.reScanBtn.setOnClickListener(v -> onQRReScanButton());
     successBottomSheetBehavior.addBottomSheetCallback(successBottomSheetCallback);
     failedBottomSheetBehavior.addBottomSheetCallback(failedBottomSheetCallback);
+    initialiseBarcodeDetector();
   }
 
-  private void startScan() {
+  @Override
+  protected void processControllerKeyData(String command) {}
+
+  @Override
+  protected void processUSBData(String data) {}
+
+  @Override
+  protected void processFrame(Bitmap image, ImageProxy imageProxy) {
+    startBarcodeDetection(image);
+  }
+
+  private void initialiseBarcodeDetector() {
     barcodeDetector =
         new BarcodeDetector.Builder(requireContext())
             .setBarcodeFormats(Barcode.ALL_FORMATS)
             .build();
-
-    cameraSource =
-        new CameraSource.Builder(requireContext(), barcodeDetector)
-            .setRequestedPreviewSize(2340, 1080)
-            .setAutoFocusEnabled(true)
-            .build();
-
-    surfaceView
-        .getHolder()
-        .addCallback(
-            new SurfaceHolder.Callback() {
-              @Override
-              public void surfaceCreated(SurfaceHolder holder) {
-                requestPermissionLauncher.launch(Manifest.permission.CAMERA);
-              }
-
-              @Override
-              public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {}
-
-              @Override
-              public void surfaceDestroyed(SurfaceHolder holder) {
-                cameraSource.stop();
-              }
-            });
-
-    barcodeDetector.setProcessor(
-        new Detector.Processor<Barcode>() {
-          @Override
-          public void release() {}
-
-          @Override
-          public void receiveDetections(@NonNull Detector.Detections<Barcode> detections) {
-            final SparseArray<Barcode> barcodes = detections.getDetectedItems();
-            if (barcodes.size() != 0 && barCodeAccess) {
-              barCodeAccess = false;
-              String barCodeValue = barcodes.valueAt(0).displayValue;
-              getFileID(barCodeValue);
-            }
-          }
-        });
   }
 
-  ActivityResultLauncher<String> requestPermissionLauncher =
-      registerForActivityResult(
-          new ActivityResultContracts.RequestPermission(),
-          new ActivityResultCallback<>() {
-            @Override
-            public void onActivityResult(Boolean result) {
-              if (result) {
-                try {
-                  cameraSource.start(surfaceView.getHolder());
-                } catch (IOException e) {
-                  throw new RuntimeException(e);
-                }
-              }
-            }
-          });
+  private void startBarcodeDetection(Bitmap image) {
+    Frame frame = null;
+    if (image != null) {
+      frame = new Frame.Builder().setBitmap(image).build();
+    }
+    SparseArray<Barcode> barcodes = barcodeDetector.detect(frame);
+    if (barcodes.size() != 0 && barCodeAccess) {
+      barCodeAccess = false;
+      String barCodeValue = barcodes.valueAt(0).displayValue;
+      getFileID(barCodeValue);
+    }
+  }
 
   private BottomSheetBehavior.BottomSheetCallback successBottomSheetCallback =
       new BottomSheetBehavior.BottomSheetCallback() {
         @Override
         public void onStateChanged(@NonNull View bottomSheet, int newState) {
-          // Handle state changes here
+          // Handle state changes here of bottomSheet.
           if (newState == BottomSheetBehavior.STATE_EXPANDED
               || newState == BottomSheetBehavior.STATE_HALF_EXPANDED) {
-            overlayView.setVisibility(View.VISIBLE);
+            binding.overlayView.setVisibility(View.VISIBLE);
           } else if (newState == BottomSheetBehavior.STATE_HIDDEN) {
-            overlayView.setVisibility(View.GONE);
+            binding.overlayView.setVisibility(View.GONE);
+            barCodeAccess = true;
           }
         }
 
@@ -162,9 +115,10 @@ public class BarCodeScannerFragment extends Fragment {
           // Handle state changes here
           if (newState == BottomSheetBehavior.STATE_EXPANDED
               || newState == BottomSheetBehavior.STATE_HALF_EXPANDED) {
-            overlayView.setVisibility(View.VISIBLE);
+            binding.overlayView.setVisibility(View.VISIBLE);
           } else {
-            overlayView.setVisibility(View.GONE);
+            binding.overlayView.setVisibility(View.GONE);
+            barCodeAccess = true;
           }
         }
 
