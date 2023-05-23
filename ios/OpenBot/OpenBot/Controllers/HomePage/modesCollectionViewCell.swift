34b486d3a1918369b8a3af8d74b3772d7bb0cf7a
Quentin Leboutet <quentin.leboutet@tum.de>
2023-05-23 20:03:09 +0200

    Harmonizing UI

--- ios/OpenBot/OpenBot/Controllers/HomePage/modesCollectionViewCell.swift
+++ ios/OpenBot/OpenBot/Controllers/HomePage/modesCollectionViewCell.swift
@@ -5,9 +5,34 @@
 import UIKit
 
 class modesCollectionViewCell: UICollectionViewCell {
-    @IBOutlet var icon: UIImageView!
-    @IBOutlet weak var title: UILabel!
+    var icon: UIImageView!
+    var title: UITextView!
+    var color: UIColor!
     static var identifier = "modesCollectionViewCell"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        icon = UIImageView(frame: CGRect(x: 0, y: frame.size.height/4, width: frame.size.width, height: frame.size.height/2))
+        icon.contentMode = .scaleAspectFit
+        contentView.addSubview(icon)
+        
+        title = UITextView(frame: CGRect(x: 0, y: frame.size.height, width: frame.size.width, height: 50))
+        title.isEditable = false
+        title.isScrollEnabled = false
+        title.textAlignment = .left
+        title.font = UIFont.systemFont(ofSize: 14)
+        
+        contentView.addSubview(title)
+        
+        // Set background color of cell
+        self.contentView.backgroundColor = color
+        self.contentView.layer.cornerRadius = 10
+    }
+        
+        required init?(coder aDecoder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -16,6 +41,8 @@ class modesCollectionViewCell: UICollectionViewCell {
     public func configure(with gridItem: ModeItem) {
         icon.image = gridItem.icon;
         title.text = gridItem.label;
+        color = gridItem.color;
+        self.contentView.backgroundColor = color
     }
 
     static func nib() -> UINib {
