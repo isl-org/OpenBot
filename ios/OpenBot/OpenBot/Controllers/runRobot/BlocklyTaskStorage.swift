984f53c65d1ccdbbd1bacb2f23fbf7f19b91aa0a
Hardik Garg <>
2023-09-07 13:05:34 +0530

    add task storage file

---
+++ ios/OpenBot/OpenBot/Controllers/runRobot/BlocklyTaskStorage.swift
@@ -0,0 +1,38 @@
+//
+//  BlocklyTaskStorage.swift
+//  OpenBot
+//
+//  Created by Hardik Garg on 07/09/23.
+//
+
+import Foundation
+
+class taskStorage {
+
+    static var array: [[String: String]] = [];
+
+    static func addAttribute(classType: String, task: String) {
+        let taskDict = [classType: task]
+        array.append(taskDict)
+    }
+
+    static func removeAttribute(classTye: String) {
+        array.removeAll { item in
+            item.keys.contains(classTye)
+        }
+    }
+
+    static func getValueOfAttribute(classType: String) -> String? {
+        for i in array {
+            if let value = i[classType] {
+                return value;
+            }
+        }
+        return nil;
+    }
+
+    static func returnAttributeArray() -> [[String: String]] {
+        array
+    }
+
+}
