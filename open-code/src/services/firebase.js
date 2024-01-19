46658f6368693cb454fba8613fae27a6d8ac2d80
Hardik Garg <>
2024-01-19 15:26:12 +0530

    fix dob issue in profile

--- open-code/src/services/firebase.js
+++ open-code/src/services/firebase.js
@@ -90,12 +90,15 @@ export async function getDateOfBirth() {
     const docRef = doc(db, "users", auth.currentUser?.uid);
     const docSnap = await getDoc(docRef);
     if (docSnap.exists()) {
-        const date = new Date(docSnap.data().dob.toDate());
-        const year = date.getFullYear();
-        const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based, so we add 1
-        const day = String(date.getDate()).padStart(2, '0');
-        return `${year}-${month}-${day}`;
+        if(docSnap.data()?.dob) {
+            const date = new Date(docSnap.data().dob.toDate());
+            const year = date.getFullYear();
+            const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based, so we add 1
+            const day = String(date.getDate()).padStart(2, '0');
+            return `${year}-${month}-${day}`;
+        }
     }
+    return undefined
 }
 
 /**
