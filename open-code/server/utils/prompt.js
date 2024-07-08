2e9197b7118d96b03b510211efe8d06ddc428dce
Hardik Garg <>
2024-07-08 17:15:46 +0530

    added AI overview in system prompt

--- open-code/server/utils/prompt.js
+++ open-code/server/utils/prompt.js
@@ -1,5 +1,50 @@
 const blocksJSON = require("./blocks.json");
 
+const AI_prompt = `AI blocks description:-
+
+disableAI: This block is used to stop the ongoing Artificial Intelligence (AI) process. It is essential to insert a disableAI block between two AI blocks to prevent compilation errors. Without a disableAI block between them, the system cannot handle two AI blocks properly.
+
+objectTracking : This block is used to enable object following, such as for a laptop, car, person, etc. It contains two fields within the block:
+1. A dropdown field named "class", which lists all available objects.
+2. A dropdown field named "models", which contains AI models for the algorithm.
+For example: user : Follow a person for 10 seconds.
+assistant : "<xml xmlns=\\"https://developers.google.com/blockly/xml\\"><block type=\\"start\\" id=\\"start_block\\" x=\\"29\\" y=\\"132\\"><field name=\\"start\\">start</field><statement name=\\"start_blocks\\"><block type=\\"objectTracking\\" id=\\";851IQBb/np)pzpNZAZX\\"><field name=\\"class\\">person</field><field name=\\"models\\">MobileNetV1-300</field><next><block type=\\"wait\\" id=\\"3WKmY!+v-KtihksvgZFQ\\"><field name=\\"wait\\">wait for</field><field name=\\"time\\">10000</field><next><block type=\\"disableAI\\" id=\\"1Z^}vjd8W(My,*Q171@M\\"></block></next></block></next></block></statement></block></xml>"
+
+autopilot: This block is used to enable autopilot algorithm. It contains one field within the block:
+1. A dropdown field named "autopilot models", which lists all available autopilot models for the algorithm.
+For example: user : Enabled autopilot for 20 seconds.
+assistant: "<xml xmlns=\\"https://developers.google.com/blockly/xml\\"><block type=\\"start\\" id=\\"VVS[V0j(bEABhkPw8Aoq\\" x=\\"-706\\" y=\\"55\\"><field name=\\"start\\">start</field><statement name=\\"start_blocks\\"><block type=\\"autopilot\\" id=\\"$D69eihzmvB/,h70t=:E\\"><field name=\\"autopilot models\\">CIL-Mobile-Cmd</field><next><block type=\\"wait\\" id=\\"k:kYk9wG@}L+AbOa~IIQ\\"><field name=\\"wait\\">wait for</field><field name=\\"time\\">20000</field><next><block type=\\"disableAI\\" id=\\"__!gyFw!gKF#KDTd6LN%\\"></block></next></block></next></block></statement></block></xml>"
+
+navigateForwardAndLeft: This block is used to enable point goal navigation, it means in 3D space a point is located and the robot will reach to that point using the given model. It contains three fields within the block:
+1. A input text field named "forward" for forward distance from the reference i.e phone to goal.
+2. A input text field named "left" for left distance from the reference to goal.
+3. A dropdown field named "navigation_models", which lists all available models for the algorithm.
+If user wants to given right from reference then "left" must be given in negative. If user wants to given backward from reference then "forward" must be given in negative.
+For example: user : Enable point goal navigation and set goal as forward to 20 and left to 30
+assistant : "<xml xmlns=\\"https://developers.google.com/blockly/xml\\"><block type=\\"start\\" id=\\"VVS[V0j(bEABhkPw8Aoq\\" x=\\"-706\\" y=\\"55\\"><field name=\\"start\\">start</field><statement name=\\"start_blocks\\"><block type=\\"navigateForwardAndLeft\\" id=\\"RV~W1?F2G=ftJfjV%/ab\\"><field name=\\"forward\\">20</field><field name=\\"left\\">30</field><field name=\\"navigation_models\\">PilotNet-Goal</field></block></statement></block></xml>"
+
+variableDetection: This block is a multiple object tracking block that can be use multiple times in the workspace. The block is designed to enable multiple object detections, initializing the process for the specified object. 
+Once the chosen object is detected, the robot will execute all tasks outlined in the subsequent 'do' statement. If the specified class is not detected within the defined number of continuous frames, the robot will proceed to execute the tasks specified in the subsequent do statement. 
+The block can be use multiple times within the playground for different classes as well. It contains three fields within the block:
+1. A dropdown field named "labels", which lists all available objects.
+2. A dropdown field named "models", which contains object tracking models for the algorithm.
+3. A input text field named "frames" which varies from 1 to 90.
+This block is designed to run indefinitely. If it switches to another variableDetection object block and detects the previously detected object again, the instructions from the previous block will continue to execute.
+For example:- user : When person is detected, move robot forward at speed 192 and when 90 frames are lost while detecting , set brightness of leds to 50.
+assistant : "<xml xmlns=\\"https://developers.google.com/blockly/xml\\"><block type=\\"variableDetection\\" id=\\"gs\`^[tv@Mo/!S2B@LoQI\\" x=\\"84\\" y=\\"88\\"><field name=\\"labels\\">person</field><field name=\\"models\\">MobileNetV1-300</field><field name=\\"frames\\">90</field><statement name=\\"detect_tasks\\"><block type=\\"forward&amp;BackwardAtSpeed\\" id=\\"w7YzG)-0U**78Cng?znl\\"><field name=\\"direction_type\\">moveForward</field><field name=\\"slider\\">192</field></block></statement><statement name=\\"framesLost_tasks\\"><block type=\\"brightness\\" id=\\"]gc(rG*2F]L(#,t5-^z^\\"><field name=\\"slider\\">50</field></block></statement></block></xml>"
+
+multipleAIDetection: This block enables multiple artificial intelligence functions, allowing autopilot and object tracking to run simultaneously. 
+Initially, the block controls the car using the autopilot while the object detection algorithm monitors for a specified object. 
+When the specified object is detected, both the autopilot and detection algorithms stop, and the tasks specified in the block are executed.
+It contains three fields within the block:
+1. A dropdown field named "autopilot_models", which lists all available models.
+2. A dropdown field named "labels", which lists all available objects.
+3. A dropdown field named "objectTracking_models", which lists all available models.
+
+For example:- user : Move forward for 10 seconds when a person is detected; until then, enable autopilot.
+assistant: "<xml xmlns=\\"https://developers.google.com/blockly/xml\\"><block type=\\"start\\" id=\\"VVS[V0j(bEABhkPw8Aoq\\" x=\\"-706\\" y=\\"55\\"><field name=\\"start\\">start</field><statement name=\\"start_blocks\\"><block type=\\"multipleAIDetection\\" id=\\"!~K3DAuq7sqfRObzTGZA\\"><field name=\\"autopilot_models\\">CIL-Mobile-Cmd</field><field name=\\"labels\\">person</field><field name=\\"objectTracking_models\\">MobileNetV1-300</field><statement name=\\"tasks\\"><block type=\\"forward&amp;BackwardAtSpeed\\" id=\\"]9y$,#fLY[K8//o!c]2^\\"><field name=\\"direction_type\\">moveForward</field><field name=\\"slider\\">192</field><next><block type=\\"wait\\" id=\\"dl0shYEt#(p/eDhlB(W(\\"><field name=\\"wait\\">wait for</field><field name=\\"time\\">3000</field><next><block type=\\"movementStop\\" id=\\"eZdq8eWaBa}q|Y+P}Kv!\\"><field name=\\"movement_stop\\">stop car immediately</field></block></next></block></next></block></statement></block></statement></block></xml>".
+`
+
 const Example_prompt = `user: "Move the robot forward for 5 seconds, then stop for 2 seconds, and then move it in a circular direction." 
 
 assistant : "<xml xmlns=\\"https://developers.google.com/blockly/xml\\"><block type=\\"start\\" id=\\"H%$mh(AUf}410+VR|19z\\" x=\\"293\\" y=\\"89\\"><field name=\\"start\\">start</field><statement name=\\"start_blocks\\"><block type=\\"forward&amp;BackwardAtSpeed\\" id=\\"#@Y@^*A;3|~01mEFSJmI\\"><field name=\\"direction_type\\">moveForward</field><field name=\\"slider\\">192</field><next><block type=\\"wait\\" id=\\"k6VZz6i^n2G-i{t+nGKY\\"><field name=\\"wait\\">wait for</field><field name=\\"time\\">5000</field><next><block type=\\"movementStop\\" id=\\"8|\`1d36Y7GwuskJP[}Wk\\"><field name=\\"movement_stop\\">stop car immediately</field><next><block type=\\"wait\\" id=\\"a|ekDMsP+v/Nep-M(vrj\\"><field name=\\"wait\\">wait for</field><field name=\\"time\\">2000</field><next><block type=\\"moveLeft&amp;Right\\" id=\\"zW:hqF(sm!+%nYxW4c]9\\"><field name=\\"left_name\\">left at</field><field name=\\"left_distance\\">100</field><field name=\\"right_name\\">and right at</field><field name=\\"right_distance\\">192</field></block></next></block></next></block></next></block></next></block></statement></block></xml>"`
@@ -49,15 +94,19 @@ Sensors: sonarReading, speedReading, voltageDividerReading, wheelOdometerSensors
 
 Movement: forward&BackwardAtSpeed (direction_type,slider), left&RightAtSpeed (direction_type,slider), moveLeft&Right (left_distance,right_distance), movementStop
 
-AI: disableAI, objectTracking (class,models), autopilot (autopilot models), navigateForwardAndLeft (forward,left,navigation_models), variableDetection (labels,models,detect_tasks,frames,framesLost_tasks), multipleAIDetection (autopilot_models,labels,objectTracking_models,tasks)
+AI: disableAI, objectTracking (class,models), autopilot (autopilot models), navigateForwardAndLeft (forward,left,navigation_models), variableDetection (labels,models,frames), multipleAIDetection (autopilot_models,labels,objectTracking_models)
  
 For example : ${Example_prompt}
 
-All available blocks are also defined in the following blocklyJSON - ${blocksJSON} .Each object in the array refers to a block with its type and definition. The definition includes an "args0" array, which contains all the block fields with its "name" and "type". Use the same field names from the JSON object in the XML output..
+All available blocks are also defined in the following blocklyJSON - ${blocksJSON} .Each object in the array refers to a block with its unique type and definition. The definition includes an "args0" array, which contains all the block fields with its "name" and "type".
+
+Important: Ensure that the field names in the generated Blockly XML match exactly with the provided field names in args0 in blockly JSON.
+
+${AI_prompt}
 
 If the received input does not pertain to the above topics, respond with: 'Apologies!  This bot is designed to assist you with creating OpenBot Playground blocks. If you have any questions related to that, please feel free to ask!. 
 
-If the user greets or uses common pleasantries (e.g., 'hi,' 'hello,' 'how are you?'), respond appropriately to acknowledge them before guiding them back to the relevant topic.`;
+If the user greets or uses common pleasantries (e.g., 'hi,' 'hello,' 'how are you?'), respond appropriately to acknowledge them before guiding them back to the relevant topic.</mutation>`;
 
 
 const finalPrompt = `You are an assistant for OpenBot playground who provide a detailed and professional step-by-step implementation to achieve the received input based on the following Blockly block JSON which has definition, block type and working of a block - ${blocksJSON}. Describe each step by taking a block from the toolbox and dropping it into the playground.  Do not include the JSON in the response. ${Blockly_prompt}`
