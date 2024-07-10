13187187e5e8073b7492a49687177e3c0a75fd4b
Hardik Garg <>
2024-07-10 16:09:59 +0530

    moved xml genration to the end of response

--- open-code/server/utils/prompt.js
+++ open-code/server/utils/prompt.js
@@ -1,8 +1,6 @@
 const blocksJSON = require("./blocks.json");
 const {custom_blocks_prompt, AI_prompt} = require("./blocksPrompt");
 
-const suggested_blocks_prompt = `After creating the XML, you can suggest various additional blocks that can be added to the playground to enhance the given input.`
-
 const example_prompt = `user: "Move the robot forward for 5 seconds, then stop for 2 seconds, and then move it in a circular direction." 
 
 assistant : "<xml xmlns=\\"https://developers.google.com/blockly/xml\\"><block type=\\"start\\" id=\\"H%$mh(AUf}410+VR|19z\\" x=\\"293\\" y=\\"89\\"><field name=\\"start\\">start</field><statement name=\\"start_blocks\\"><block type=\\"forward&amp;BackwardAtSpeed\\" id=\\"#@Y@^*A;3|~01mEFSJmI\\"><field name=\\"direction_type\\">moveForward</field><field name=\\"slider\\">192</field><next><block type=\\"wait\\" id=\\"k6VZz6i^n2G-i{t+nGKY\\"><field name=\\"wait\\">wait for</field><field name=\\"time\\">5000</field><next><block type=\\"movementStop\\" id=\\"8|\`1d36Y7GwuskJP[}Wk\\"><field name=\\"movement_stop\\">stop car immediately</field><next><block type=\\"wait\\" id=\\"a|ekDMsP+v/Nep-M(vrj\\"><field name=\\"wait\\">wait for</field><field name=\\"time\\">2000</field><next><block type=\\"moveLeft&amp;Right\\" id=\\"zW:hqF(sm!+%nYxW4c]9\\"><field name=\\"left_name\\">left at</field><field name=\\"left_distance\\">100</field><field name=\\"right_name\\">and right at</field><field name=\\"right_distance\\">192</field></block></next></block></next></block></next></block></next></block></statement></block></xml>"`
@@ -53,7 +51,7 @@ Sensors: sonarReading, speedReading, voltageDividerReading, wheelOdometerSensors
 Movement: forward&BackwardAtSpeed, left&RightAtSpeed, moveLeft&Right, movementStop
 
 AI: disableAI, objectTracking, autopilot, navigateForwardAndLeft, variableDetection, multipleAIDetection
- 
+
 All available blocks are also defined in the following blocklyJSON - ${blocksJSON} .Each object in the array refers to a block with its unique type and definition. The definition includes an "args0" array, which contains all the block fields with its "name" and "type".
 
 Important: Ensure that the field names in the generated Blockly XML match exactly with the provided field names in args0 in blockly JSON.
@@ -64,8 +62,6 @@ ${custom_blocks_prompt}
 
 ${AI_prompt}
 
-${suggested_blocks_prompt}
-
 If the received input does not pertain to the above topics, respond with: 'Apologies!  This bot is designed to assist you with creating OpenBot Playground blocks. If you have any questions related to that, please feel free to ask!. 
 
 If the user greets or uses common pleasantries (e.g., 'hi,' 'hello,' 'how are you?'), respond appropriately to acknowledge them before guiding them back to the relevant topic.</mutation>`;
@@ -75,7 +71,7 @@ const response_structure = `Explain the process according to following rules :
 
 1. Explain the process of dragging each block from the toolbox and dropping it into the playground. Provide this explanation for each block.
 2. Include a description of the use case for each block.
-
+3. Suggest various additional blocks that can be added to the playground to enhance the given input.
 IMPORTANT NOTE: Ensure that all responses respect this above constraint.
 `
 
