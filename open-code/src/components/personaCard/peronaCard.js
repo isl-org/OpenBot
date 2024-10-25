7598cd494c86e8aab7d9096a85b6c4e301bf26f9
isha382 <isha.chaudhary@itinker.io>
2024-10-25 17:18:59 +0530

    added functionality to show selection effect of persona

--- open-code/src/components/personaCard/peronaCard.js
+++ open-code/src/components/personaCard/peronaCard.js
@@ -1,34 +1,48 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import styles from "./personaCard.module.css";
 import { personas } from "../../utils/persona";
 import { ThemeContext } from "../../App";
 import { Themes } from "../../utils/constants";
-import {Images} from "../../utils/images";
+import { Images } from "../../utils/images";
 
 /**
- * component for choosing persona character
+ * Component for choosing persona character
  * @param handlePersonas
  * @returns {Element}
  * @constructor
  */
 const PersonaCard = ({ handlePersonas }) => {
     const theme = useContext(ThemeContext);
+    const [selectedPersona, setSelectedPersona] = useState(null); // State to track selected persona
+
+    const handleClick = (key) => {
+        if (!selectedPersona) { // Only allow click if no persona is selected
+            setSelectedPersona(key);
+            handlePersonas(key);
+        }
+    };
 
     return (
         <div>
             <div className={styles.headingContainer}>
                 <img className={styles.icon}
-                    alt={ "Person Icon"}
+                     alt={"Person Icon"}
                      src={theme.theme === Themes.dark ? Images.darkUserIcon : Images.userIcon}
                 />
-                <h3>Choose Your Persona : </h3>
+                <h3 style={{fontSize: '16px'}}>Choose Your Persona:</h3>
             </div>
             <div className={`${theme.theme === Themes.dark ? styles.dark : ''} ${styles.personaContainer}`}>
-                {personas.slice(0, 5).map((item, id) => (
+                {personas.slice(0, 5).map((item) => (
                     <div
-                        key={id}
-                        className={`${theme.theme === Themes.dark ? styles.dark : ''} ${styles.personaCard}`}
-                        onClick={() => handlePersonas(item.key)}
+                        key={item.key}
+                        className={`${theme.theme === Themes.dark ? styles.dark : ''} ${styles.personaCard} 
+                          ${selectedPersona === item.key ? styles.selected : ''}`}
+                        onClick={() => handleClick(item.key)}
+                        style={{
+                         fontSize: '14px',
+                            pointerEvents: selectedPersona ? 'none' : 'auto',
+                            cursor: selectedPersona ? 'not-allowed' : 'pointer'
+                        }}
                     >
                         <h4>{item.name}</h4>
                     </div>
